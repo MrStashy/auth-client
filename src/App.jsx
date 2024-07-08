f121ef5d6832352735421d5ef2d8b5f032527d64
Will Baxter <bax.will@googlemail.com>
2024-07-08 12:09:28 +0100

    Form HTML built

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css';
+import { useState } from 'react';
 
 export default function App() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: ""
+  })
+
   return (
-    <div className="App">
+    <div className="App p-2">
     <p className="text-4xl">Mighty Authin' Power Rangers</p>
+    <form className="flex flex-col border-2 p-2 gap-2">
+      <label htmlFor="username">Username</label>
+      <input name="username" type="textbox" className="border w-44"/>
+      <label htmlFor="password">Password</label>
+      <input name="password" type="textbox" className="border w-44"/>
+      <button className="w-44 h-8 bg-slate-300 rounded-md mt-2">Submit</button>
+    </form>
     </div>
   );
 }
