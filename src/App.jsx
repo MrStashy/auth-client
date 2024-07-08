533d56a778b8afa8a56ee94b3ab7e3376a741395
Will Baxter <bax.will@googlemail.com>
2024-07-08 12:15:37 +0100

    React form functionality

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,26 @@ export default function App() {
     password: ""
   })
 
+  function handleChange (e) {
+    const { name, value } = e.target
+    setFormData({...formData, [name]: value})
+  }
+
+  function handleSubmit (e) {
+
+  }
+
   return (
     <div className="App p-2">
     <p className="text-4xl">Mighty Authin' Power Rangers</p>
-    <form className="flex flex-col border-2 p-2 gap-2">
+    <form onChange={handleChange} onSubmit={handleSubmit} className="flex flex-col p-2 gap-2">
       <label htmlFor="username">Username</label>
-      <input name="username" type="textbox" className="border w-44"/>
+      <input name="username" type="textbox" className="border w-44" value={formData.username}/>
       <label htmlFor="password">Password</label>
-      <input name="password" type="textbox" className="border w-44"/>
+      <input name="password" type="password" className="border w-44" value={formData.password}/>
       <button className="w-44 h-8 bg-slate-300 rounded-md mt-2">Submit</button>
     </form>
     </div>
   );
 }
+ 
